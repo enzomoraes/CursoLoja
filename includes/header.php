6e1c39edbe856c531d448b03a03ef00525300dc8
Enzo Moraes <enzomoraes12@hotmail.com>
2020-03-02 21:31:55 -0400

    adicionando favicon

--- includes/header.php
+++ includes/header.php
@@ -10,6 +10,9 @@
       <link rel="stylesheet" href="css/style.css">
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <link rel="shortcut icon" href="images/favicon.jpg" />
+
+
     </head>
 
 <body>
